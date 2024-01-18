457742fa673074654b1bf60362eda8201627b483
betalantz <betalantz@gmail.com>
2024-01-17 16:07:22 -0800

    implements sort

--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Search from './Search'
 import AddTrackForm from './AddTrackForm'
 import TracksList from './TracksList'
+import Sort from './Sort'
+import { unstable_renderSubtreeIntoContainer } from 'react-dom'
 
 function TracksPage() {
 
   const [tracks, setTracks] = useState([])
   const [searchTerm, setSearchTerm] = useState("")
+  const [sortBy, setSortBy] = useState("artist")
 
   useEffect(() => {
     fetch("http://localhost:8001/tracks")
@@ -23,10 +26,18 @@ function TracksPage() {
     return track.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
             track.artist.toLowerCase().includes(searchTerm.toLowerCase())
   })
+    .sort((track1, track2) => {
+      if (sortBy === 'artist') {
+        return track1.artist.localeCompare(track2.artist)
+      } else {
+        return track1.BPM - track2.BPM
+      }
+    })
   
     
   return (
     <div>
+      <Sort sortBy={sortBy} onChangeSort={setSortBy} />
       <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
       <AddTrackForm onSubmitForm={addTrack}/>
       <TracksList tracks={filteredTracks} />
